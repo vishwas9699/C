1f7bd7860dae2c8246c6339b578d5861f31435e8
vishwas9699 <vvishwas179@gmail.com>
2021-03-28 20:18:36 +0530

    ic

--- C/main1.c
+++ C/main1.c
@@ -4,21 +4,12 @@
 int main(){
 
     int countr=1;
-    
-    while (countr<=10){
-        printf("The natural Numbers are %d \n",countr);
+
+    do {
+        printf("%d\n",countr);
         countr++;
-    }
-
-    int num=5;
-    long factorial = 1;
-
-    while (num>0){
-        factorial=factorial*num;
-        num--;
-    }
-
-    printf("Factorial is: %ld ", factorial);
+    } while (countr<=10);
+    
    
     return 0;
 }
