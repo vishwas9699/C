2a1eefc0e7812650a90ebcc792e0a7faf08e1219
vishwas9699 <vvishwas179@gmail.com>
2021-03-28 08:44:15 +0530

    ic

---
+++ C/main1.c
@@ -0,0 +1,31 @@
+// Loops
+
+#include<stdio.h>
+int main(){
+    int number=1;
+    if (number %2 !=0)
+    printf("%d\n",number);
+
+    number++;
+    if (number %2 !=0)
+    printf("%d\n",number);
+
+    number++;
+
+    if (number %2 !=0)
+    printf("%d\n",number);
+
+    number++;   
+
+    if (number %2 !=0)
+    printf("%d\n",number);
+
+    number++;
+
+    if (number %2 !=0)
+    printf("%d\n",number);
+
+    number++;
+
+    return 0;
+}
