ac9bfbbd0ee41ef42503e8a5e06f6d91f75bc97f
vishwas9699 <vvishwas179@gmail.com>
2021-03-30 18:22:34 +0530

    ic

--- C/main1.c
+++ C/main1.c
@@ -3,22 +3,6 @@
 #include<stdio.h>
 int main(){
 
-    int countr=1;
-
-    do {
-        printf("%d\n",countr);
-        countr++;
-    } while (countr<=10);
-
-    float num,sum;
-    do{
-        puts("Enter the number: ");
-        scanf("%f",&num);
-        sum += num;
-    } while(num!=0);
-    
-    printf("The sum is: %.3f",sum);
-    
     
     return 0;
 }
